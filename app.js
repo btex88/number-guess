0bbebc366c603d2314a64d9e3112435fac219404
bruno teixeira <btex88@gmail.com>
2022-03-08 23:31:48 -0300

    feat: add logic to display led segments based on result value / add rematch button / logict to handle error / logic to react if  number is equals as result, greater and smaller

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
+// Constants
+const numbersArr = [
+  ['a', 'b', 'c', 'e', 'f', 'g'],
+  ['e', 'f'],
+  ['a', 'e', 'd', 'c', 'g'],
+  ['a', 'e', 'd', 'f', 'g'],
+  ['b', 'e', 'd', 'f'],
+  ['a', 'b', 'd', 'f', 'g'],
+  ['a', 'b', 'c', 'd', 'f', 'g'],
+  ['a', 'e', 'f'],
+  ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
+  ['a', 'b', 'd', 'e', 'f', 'g'],
+];
+
+const api = {
+  url: 'https://us-central1-ss-devops.cloudfunctions.net/rand?min=1&max=300',
+
+  get: () =>
+    fetch(api.url)
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((err) => err),
+};
+
+const result = {};
+
+const resultMessage = document.querySelector('.result_message');
+const inputElement = document.getElementById('number_input');
+const digit2 = document.querySelector('.digit_2');
+const digit3 = document.querySelector('.digit_3');
+const btnElement = document.getElementById('send_button');
+
+// Function to limit the amount of value typed at input field
+function limitInputLength(input) {
+  const THREE = 3;
+  const THREE_HUNDRED = 300;
+  const ONE = 1;
+  const ZERO = 0;
+  if (input.value.length > THREE) input.value = input.value.slice(0, THREE);
+  if (input.value > THREE_HUNDRED) input.value = THREE_HUNDRED;
+  if (input.value < ZERO) input.value = ONE;
+}
+
+// Function to handle value and digits to be displayed at LED segments
 function setNumberOn(arr, digit) {
   arr.forEach((value) => {
     const element = document.querySelector(`.digit_${digit} .segment_${value}`);
@@ -5,46 +49,91 @@ function setNumberOn(arr, digit) {
   });
 }
 
+// Function to clear LED segments colors, appearance and result message
 function clearAll() {
   const hiddenElements = document.querySelectorAll('.hide');
   const onElements = document.querySelectorAll('.on');
+  const redElements = document.querySelectorAll('.red');
+
   hiddenElements.forEach((element) => element.classList.remove('hide'));
   onElements.forEach((element) => element.classList.remove('on'));
+  redElements.forEach((element) => element.classList.remove('red'));
+  resultMessage.classList.remove('error');
 }
 
+// Function to handle input values
 function handleInputValue(num) {
+  clearAll();
   const numArr = num.toString().split('');
 
   if (numArr.length === 1) {
-    const digit2 = document.querySelector('.digit_2');
-    const digit3 = document.querySelector('.digit_3');
     digit2.classList.add('hide');
     digit3.classList.add('hide');
   }
 
   if (numArr.length === 2) {
-    const digit3 = document.querySelector('.digit_3');
     digit3.classList.add('hide');
   }
-
   numArr.forEach((value, index) => setNumberOn(numbersArr[value], index + 1));
 }
 
+function handleRightAnswer() {
+  const allElements = document.querySelectorAll('.on');
+  allElements.forEach((element) => {
+    element.classList.remove('on');
+    element.classList.add('green');
+  });
+  resultMessage.classList.add('equal');
+}
+
+function checkResult() {
+  if (Number(inputElement.value) > result.value) {
+    resultMessage.classList.add('greater');
+  } else if (Number(inputElement.value) < result.value) {
+    resultMessage.classList.add('smaller');
+  } else {
+    handleRightAnswer();
+  }
+}
+
+function clearResultMessage() {
+  resultMessage.classList.remove('greater');
+  resultMessage.classList.remove('smaller');
+}
+
+// Function to handle send button click
 function handleButtonClick() {
-  clearAll();
-  const inputElement = document.getElementById('number_input');
-  if (inputElement.value) {
-    handleInputValue(inputElement.value);
-  } else setNumberOn(numbersArr[0]);
+  clearResultMessage();
+  if (inputElement.value) handleInputValue(inputElement.value);
+  else setNumberOn(numbersArr[0]);
+  checkResult();
   inputElement.value = '';
+  console.log(result.value)
 }
 
-function getInputNumberOnClick() {
-  const btnElement = document.getElementById('send_button');
-  btnElement.addEventListener('click', handleButtonClick);
+// Function to add event listeners for button element
+function addEventToButton() {
+  btnElement.addEventListener('click', () => handleButtonClick());
 }
 
+// Function to handle error on api request
+function handleError(value) {
+  handleInputValue(value);
+  const allElements = document.querySelectorAll('.on');
+  allElements.forEach((element) => {
+    element.classList.remove('on');
+    element.classList.add('red');
+  });
+  resultMessage.classList.add('error');
+}
+
+// Load this before browser load page
 window.onload = () => {
-  getInputNumberOnClick();
   handleInputValue(0);
+  addEventToButton();
+  api.get().then((data) => {
+    data.StatusCode
+      ? handleError(data.StatusCode)
+      : (result.value = data.value);
+  });
 };
